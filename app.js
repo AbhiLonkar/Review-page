ad2d1c77b1062eb57e474e4a144394c606940754
AbhiLonkar <130952856+AbhiLonkar@users.noreply.github.com>
2023-04-25 22:43:03 +0530

    Added review project

---
+++ app.js
@@ -0,0 +1,62 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    img: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    img: 'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    img: 'https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+var reviewsIndex = 0
+$(".next-btn").on("click",function(){
+  reviewsIndex++;
+  call(reviewsIndex);
+  if (reviewsIndex>=reviews.length-1){
+    reviewsIndex=-1
+  }
+})
+
+// reviewsIndex2 = (reviews.length)-1;
+$(".prev-btn").on("click",function(){
+  if (reviewsIndex<=0){
+    reviewsIndex=reviews.length;
+  }
+  reviewsIndex--;
+  call(reviewsIndex);
+})
+
+
+$(".random-btn").on("click",function(){
+  var randomNumber = Math.floor(Math.random()*(reviews.length));
+  call(randomNumber)
+})
+
+function call(person){
+  $("#author").text(reviews[person].name);
+  $("#job").text(reviews[person].job);
+  $("#person-img").attr("src",reviews[person].img);
+  $("#info").text(reviews[person].text);
+}
